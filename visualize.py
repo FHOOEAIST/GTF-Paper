4e92a57f5af8f3c0bc05f15dcbb7116a7c4ccb64
Andreas Pointner <andreas.pointner@fh-hagenberg.at>
2022-02-21 16:09:12 +0100

    updated visualization of top ten dependencies

--- visualize.py
+++ visualize.py
@@ -3,7 +3,9 @@ import pandas as pd
 
 if __name__ == '__main__':
     cnt = 10
-    df = pd.read_csv(r"C:\Public\git\_github\GTF-Paper\versionsWithVersion.txt", sep=";", nrows=cnt).sort_values('Count', ascending=True)
+    df = pd.read_csv(r"C:\Public\git\_github\GTF-Paper\versionsWithVersion.txt", sep=";")
+    df = df[df["Dependency"].str.contains(r"android|org\.jetbrains") == False]
+    df = df.head(cnt).sort_values('Count', ascending=True)
     fig = px.bar(df, x="Count", y="Dependency", orientation='h', title=f"Top {cnt} used dependencies", text="Count",
                  width=1000, height=500, labels={"Count": "Number of times dependency is (transitive) referenced", "Dependency": "Dependency <groupId>:<artifactId>"}, template="simple_white")
     fig.update_traces(marker_color='black')
